4905ec4b9d37688a67a6eddfab439e7d070b8ff8
jeancdev <jcode2006@gmail.com>
2026-06-22 16:40:45 -0500

    feat(database-package): add data source configuration for PostgreSQL connection

---
+++ packages/database/src/config/create-data-source.ts
@@ -0,0 +1,26 @@
+import { DataSource, type DataSourceOptions } from 'typeorm'
+
+import { entitySchemas } from '../entities/index.js'
+
+import { InitialSchema1740350000000 } from '../migrations/1740350000000-InitialSchema.js'
+
+import { readDatabaseEnv } from './database-env.js'
+
+export const createDataSourceOptions = (): DataSourceOptions => {
+  const env = readDatabaseEnv()
+
+  return {
+    type: 'postgres',
+    url: env.url,
+    entities: [...entitySchemas],
+    migrations: [InitialSchema1740350000000],
+    migrationsTableName: 'typeorm_migrations',
+    synchronize: false,
+    logging: env.logging
+  }
+}
+
+export const createDataSource = (): DataSource =>
+  new DataSource(createDataSourceOptions())
+
+export const dataSource = createDataSource()

23cb5455d6d692c5a412f90035f681f4553518e6
jeancdev <jcode2006@gmail.com>
2026-06-22 16:54:59 -0500

    feat(database-config): load environment variables in data source configuration

--- packages/database/src/config/create-data-source.ts
+++ packages/database/src/config/create-data-source.ts
@@ -5,6 +5,9 @@ import { entitySchemas } from '../entities/index.js'
 import { InitialSchema1740350000000 } from '../migrations/1740350000000-InitialSchema.js'
 
 import { readDatabaseEnv } from './database-env.js'
+import { loadEnv } from './load-env.js'
+
+loadEnv()
 
 export const createDataSourceOptions = (): DataSourceOptions => {
   const env = readDatabaseEnv()

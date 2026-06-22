69b25d27c3b485132a021ca378a3e6574a377d9b
jeancdev <jcode2006@gmail.com>
2026-06-22 16:40:15 -0500

    feat(database-package): create index file to export all entity schemas for easier access

---
+++ packages/database/src/entities/index.ts
@@ -0,0 +1,35 @@
+import { auditLogSchema } from './schemas/audit-log.schema.js'
+import { parkingSpotSchema } from './schemas/parking-spot.schema.js'
+import { reservationSchema } from './schemas/reservation.schema.js'
+import { rfidTagSchema } from './schemas/rfid-tag.schema.js'
+import { sensorDataSchema } from './schemas/sensor-data.schema.js'
+import { userSchema } from './schemas/user.schema.js'
+
+export { auditLogSchema, type AuditLogRow } from './schemas/audit-log.schema.js'
+export {
+  parkingSpotSchema,
+  type ParkingSpotRow
+} from './schemas/parking-spot.schema.js'
+export {
+  reservationSchema,
+  type ReservationRow
+} from './schemas/reservation.schema.js'
+export { rfidTagSchema, type RfidTagRow } from './schemas/rfid-tag.schema.js'
+export {
+  sensorDataSchema,
+  type SensorDataRow
+} from './schemas/sensor-data.schema.js'
+export {
+  userSchema,
+  type UserRole,
+  type UserRow
+} from './schemas/user.schema.js'
+
+export const entitySchemas = [
+  userSchema,
+  parkingSpotSchema,
+  reservationSchema,
+  rfidTagSchema,
+  auditLogSchema,
+  sensorDataSchema
+] as const

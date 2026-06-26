6188139506caf773a9fe24260cf421f7fdd2fa3c
jeancdev <jcode2006@gmail.com>
2026-06-25 20:30:48 -0500

    feat(dev-iac): add security group module configuration for development environment

---
+++ iac/environments/dev/security-groups.tf
@@ -0,0 +1,9 @@
+module "security_groups" {
+  source = "../../modules/security-groups"
+
+  project_name   = var.project_name
+  environment    = var.environment
+  vpc_id         = module.vpc.vpc_id
+  vpc_cidr_block = module.vpc.vpc_cidr_block
+  tags           = var.tags
+}

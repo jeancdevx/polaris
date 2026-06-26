08e1ca87375ac79b104033832738105e456e3d6a
jeancdev <jcode2006@gmail.com>
2026-06-25 19:51:12 -0500

    feat(vpc): add VPC and internet gateway resources to VPC module

---
+++ iac/modules/vpc/vpc.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-vpc"
+  })
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-igw"
+  })
+}

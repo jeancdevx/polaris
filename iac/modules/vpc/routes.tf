7f0c12e9346e5ec2629e8a06e3e5732073491340
jeancdev <jcode2006@gmail.com>
2026-06-25 22:50:13 -0500

    refactor(vpc): update NAT gateway route configuration to conditionally enable based on variable

--- iac/modules/vpc/routes.tf
+++ iac/modules/vpc/routes.tf
@@ -32,7 +32,7 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route" "private_nat" {
-  for_each = aws_route_table.private
+  for_each = var.enable_nat_gateway ? aws_route_table.private : {}
 
   route_table_id         = each.value.id
   destination_cidr_block = "0.0.0.0/0"

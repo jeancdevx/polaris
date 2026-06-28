b8ab5b8966075d691a5bc571717803a7b907f121
jeancdev <jcode2006@gmail.com>
2026-06-28 12:51:03 -0500

    refactor(secrets-manager): replace CloudFormation stack with Secrets Manager secret rotation resource

--- iac/modules/secrets-manager/rds-rotation.tf
+++ iac/modules/secrets-manager/rds-rotation.tf
@@ -1,45 +1,11 @@
-resource "aws_cloudformation_stack" "rds_secret_rotation" {
+resource "aws_secretsmanager_secret_rotation" "rds_master" {
   count = local.enable_rds_rotation ? 1 : 0
 
-  name = "${local.name_prefix}-rds-secret-rotation"
+  secret_id = var.rds_master_secret_arn
 
-  capabilities = [
-    "CAPABILITY_IAM",
-    "CAPABILITY_AUTO_EXPAND",
-  ]
+  rotation_rules {
+    automatically_after_days = local.rds_rotation_days
+  }
 
-  template_body = jsonencode({
-    AWSTemplateFormatVersion = "2010-09-09"
-    Transform                = "AWS::SecretsManager-2024-09-16"
-    Description              = "Automatic rotation for Aurora PostgreSQL master user secret"
-
-    Resources = {
-      SecretRotationSchedule = {
-        Type = "AWS::SecretsManager::RotationSchedule"
-        Properties = {
-          SecretId = var.rds_master_secret_arn
-          HostedRotationLambda = {
-            RotationType        = "PostgreSQLSingleUser"
-            RotationLambdaName  = local.rds_rotation_lambda_name
-            VpcSecurityGroupIds = join(",", var.security_group_ids)
-            VpcSubnetIds        = join(",", var.subnet_ids)
-          }
-          RotationRules = {
-            AutomaticallyAfterDays = local.rds_rotation_days
-          }
-          RotateImmediatelyOnUpdate = local.rotate_immediately
-        }
-      }
-    }
-
-    Outputs = {
-      RotationScheduleId = {
-        Value = {
-          Ref = "SecretRotationSchedule"
-        }
-      }
-    }
-  })
-
-  tags = local.common_tags
+  rotate_immediately = local.rotate_immediately
 }

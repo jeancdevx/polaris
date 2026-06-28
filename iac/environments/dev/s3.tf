39c4cf26df8c1492b1ea2b66181a418f13a1afd6
jeancdev <jcode2006@gmail.com>
2026-06-27 20:48:46 -0500

    feat(dev-iac): add S3 module configuration for development environment

---
+++ iac/environments/dev/s3.tf
@@ -0,0 +1,12 @@
+module "s3" {
+  source = "../../modules/s3"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  force_destroy                     = var.s3_force_destroy
+  kms_key_arn                       = var.s3_kms_key_arn
+  lifecycle_glacier_transition_days = var.s3_lifecycle_glacier_transition_days
+
+  tags = var.tags
+}

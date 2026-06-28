adfda729489c161cf4858af84a4ea5a3e96f8f97
jeancdev <jcode2006@gmail.com>
2026-06-27 20:13:50 -0500

    feat(cognito): add user groups for admin and regular users in Cognito

---
+++ iac/modules/cognito/groups.tf
@@ -0,0 +1,13 @@
+resource "aws_cognito_user_group" "admin" {
+  name         = "admin"
+  user_pool_id = aws_cognito_user_pool.main.id
+  description  = "Administrators with optional MFA and elevated API access"
+  precedence   = 0
+}
+
+resource "aws_cognito_user_group" "user" {
+  name         = "user"
+  user_pool_id = aws_cognito_user_pool.main.id
+  description  = "Registered parking application users"
+  precedence   = 10
+}

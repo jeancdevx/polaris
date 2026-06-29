ce0efcbe0041c8dc405b8fb790b367cf2651ee56
jeancdev <jcode2006@gmail.com>
2026-06-28 20:31:13 -0500

    feat(security-groups): add egress rules for ECS to various services including RDS, Redis, and MSK

--- iac/modules/security-groups/rules.tf
+++ iac/modules/security-groups/rules.tf
@@ -1,3 +1,63 @@
+resource "aws_security_group_rule" "alb_to_ecs" {
+  type                     = "egress"
+  security_group_id        = aws_security_group.alb.id
+  protocol                 = "tcp"
+  from_port                = var.ecs_container_port
+  to_port                  = var.ecs_container_port
+  source_security_group_id = aws_security_group.ecs.id
+  description              = "Forward traffic to ECS tasks"
+}
+
+resource "aws_security_group_rule" "ecs_egress_https_vpc" {
+  type              = "egress"
+  security_group_id = aws_security_group.ecs.id
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = [var.vpc_cidr_block]
+  description       = "HTTPS to interface VPC endpoints"
+}
+
+resource "aws_security_group_rule" "ecs_egress_https_internet" {
+  type              = "egress"
+  security_group_id = aws_security_group.ecs.id
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "HTTPS to AWS public APIs (Cognito)"
+}
+
+resource "aws_security_group_rule" "ecs_egress_rds" {
+  type                     = "egress"
+  security_group_id        = aws_security_group.ecs.id
+  protocol                 = "tcp"
+  from_port                = var.rds_port
+  to_port                  = var.rds_port
+  source_security_group_id = aws_security_group.rds.id
+  description              = "Aurora PostgreSQL"
+}
+
+resource "aws_security_group_rule" "ecs_egress_redis" {
+  type                     = "egress"
+  security_group_id        = aws_security_group.ecs.id
+  protocol                 = "tcp"
+  from_port                = var.redis_port
+  to_port                  = var.redis_port
+  source_security_group_id = aws_security_group.redis.id
+  description              = "ElastiCache Redis"
+}
+
+resource "aws_security_group_rule" "ecs_egress_msk" {
+  type                     = "egress"
+  security_group_id        = aws_security_group.ecs.id
+  protocol                 = "tcp"
+  from_port                = var.msk_client_port
+  to_port                  = var.msk_client_port
+  source_security_group_id = aws_security_group.msk.id
+  description              = "MSK IAM SASL client port"
+}
+
 resource "aws_security_group_rule" "alb_http_ingress" {
   type              = "ingress"
   security_group_id = aws_security_group.alb.id

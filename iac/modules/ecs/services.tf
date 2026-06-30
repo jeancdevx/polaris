9a372f125a65d372ad77cf163ceac2a97a1ba2ee
jeancdev <jcode2006@gmail.com>
2026-06-30 14:08:06 -0500

    feat(ecs): add ECS service configuration for event processor service with network settings and health checks

--- iac/modules/ecs/services.tf
+++ iac/modules/ecs/services.tf
@@ -75,3 +75,35 @@ resource "aws_ecs_service" "reservation_service" {
     ignore_changes = [desired_count]
   }
 }
+
+resource "aws_ecs_service" "event_processor_service" {
+  name            = local.event_processor_service_name
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.event_processor_service.arn
+  desired_count   = var.event_processor_service_desired_count
+  launch_type     = "FARGATE"
+
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+
+  network_configuration {
+    subnets          = var.private_subnet_ids
+    security_groups  = [var.ecs_security_group_id]
+    assign_public_ip = false
+  }
+
+  health_check_grace_period_seconds = 90
+
+  tags = merge(local.common_tags, {
+    Name    = local.event_processor_service_name
+    Service = "event-processor-service"
+  })
+
+  depends_on = [
+    aws_secretsmanager_secret_version.event_processor_service_env
+  ]
+
+  lifecycle {
+    ignore_changes = [desired_count]
+  }
+}

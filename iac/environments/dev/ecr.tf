59fe216a44b320d6a35ff6749e217e4082fbc921
jeancdev <jcode2006@gmail.com>
2026-06-29 19:05:40 -0500

    feat(dev-iac): rename ECR module and add reservation service configuration

--- iac/environments/dev/ecr.tf
+++ iac/environments/dev/ecr.tf
@@ -1,9 +1,19 @@
-module "ecr" {
+module "ecr_api_service" {
   source = "../../modules/ecr"
 
   project_name = var.project_name
   environment  = var.environment
   service_name = "api-service"
+
+  tags = var.tags
+}
+
+module "ecr_reservation_service" {
+  source = "../../modules/ecr"
+
+  project_name = var.project_name
+  environment  = var.environment
+  service_name = "reservation-service"
 
   tags = var.tags
 }

af0beaf1c5d5a9408da35dc2d0bc457976beeee8
jeancdev <jcode2006@gmail.com>
2026-06-29 19:50:47 -0500

    feat(event-processor-service): register new handlers and services in EventProcessorModule

--- apps/event-processor-service/src/processing/event-processor.module.ts
+++ apps/event-processor-service/src/processing/event-processor.module.ts
@@ -3,11 +3,28 @@ import { ConfigModule } from '@nestjs/config'
 
 import { eventProcessorConfig } from './event-processor.config.js'
 import { EventDispatcherService } from './event-dispatcher.service.js'
+import { SensorOccupancyHandler } from './handlers/sensor-occupancy.handler.js'
+import { VehicleEntryHandler } from './handlers/vehicle-entry.handler.js'
+import { VehicleExitHandler } from './handlers/vehicle-exit.handler.js'
+import { DatabaseService } from './infrastructure/database.service.js'
+import { RedisService } from './infrastructure/redis.service.js'
 import { KafkaConsumerService } from './kafka-consumer.service.js'
+import { ParkingRedisStore } from './parking/parking-redis.store.js'
+import { ParkingRepository } from './parking/parking.repository.js'
 
 @Module({
   imports: [ConfigModule.forFeature(eventProcessorConfig)],
-  providers: [EventDispatcherService, KafkaConsumerService],
+  providers: [
+    DatabaseService,
+    RedisService,
+    ParkingRepository,
+    ParkingRedisStore,
+    VehicleEntryHandler,
+    VehicleExitHandler,
+    SensorOccupancyHandler,
+    EventDispatcherService,
+    KafkaConsumerService
+  ],
   exports: [EventDispatcherService, KafkaConsumerService]
 })
 export class EventProcessorModule {}

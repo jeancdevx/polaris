4e4485323bfb9cfcf7a00eead98b6cdfe314f3c1
jeancdev <jcode2006@gmail.com>
2026-06-30 16:23:37 -0500

    feat(dev-iac): integrate sensor data processor module into IoT Core configuration

--- iac/environments/dev/iot-core.tf
+++ iac/environments/dev/iot-core.tf
@@ -7,9 +7,13 @@ module "iot_core" {
   rfid_validator_function_arn  = module.rfid_validator.function_arn
   rfid_validator_function_name = module.rfid_validator.function_name
 
+  sensor_data_processor_function_arn  = module.sensor_data_processor.function_arn
+  sensor_data_processor_function_name = module.sensor_data_processor.function_name
+
   tags = var.tags
 
   depends_on = [
     module.rfid_validator,
+    module.sensor_data_processor,
   ]
 }

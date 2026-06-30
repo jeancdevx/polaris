69f4757248166f98b0282d406ffe834850ce55a3
jeancdev <jcode2006@gmail.com>
2026-06-30 14:07:34 -0500

    feat(ecs): add AWS Secrets Manager secrets for event processor service environment

--- iac/modules/ecs/secrets.tf
+++ iac/modules/ecs/secrets.tf
@@ -27,3 +27,18 @@ resource "aws_secretsmanager_secret_version" "reservation_service_env" {
   secret_id     = aws_secretsmanager_secret.reservation_service_env.id
   secret_string = jsonencode(local.reservation_service_env)
 }
+
+resource "aws_secretsmanager_secret" "event_processor_service_env" {
+  name                    = "${local.name_prefix}-event-processor-service-env"
+  recovery_window_in_days = local.secret_recovery_window_days
+
+  tags = merge(local.common_tags, {
+    Name    = "${local.name_prefix}-event-processor-service-env"
+    Service = "event-processor-service"
+  })
+}
+
+resource "aws_secretsmanager_secret_version" "event_processor_service_env" {
+  secret_id     = aws_secretsmanager_secret.event_processor_service_env.id
+  secret_string = jsonencode(local.event_processor_service_env)
+}

80ad9d7d301201d5d9f9cf8757867d2dd17c9a1b
jeancdev <jcode2006@gmail.com>
2026-06-29 19:50:33 -0500

    feat(event-processor-service): extend eventProcessorConfig to include redisUrl and define EventProcessorConfig type

--- apps/event-processor-service/src/processing/event-processor.config.ts
+++ apps/event-processor-service/src/processing/event-processor.config.ts
@@ -1,7 +1,19 @@
 import { registerAs } from '@nestjs/config'
 
-export const eventProcessorConfig = registerAs('eventProcessor', () => ({
-  clientId: process.env.KAFKA_CLIENT_ID ?? 'event-processor-service',
-  consumerGroupId:
-    process.env.KAFKA_CONSUMER_GROUP_ID ?? 'event-processor-service'
-}))
+export type EventProcessorConfig = {
+  clientId: string
+  consumerGroupId: string
+  redisUrl: string
+}
+
+export const EVENT_PROCESSOR_CONFIG_KEY = 'eventProcessor'
+
+export const eventProcessorConfig = registerAs(
+  EVENT_PROCESSOR_CONFIG_KEY,
+  (): EventProcessorConfig => ({
+    clientId: process.env.KAFKA_CLIENT_ID ?? 'event-processor-service',
+    consumerGroupId:
+      process.env.KAFKA_CONSUMER_GROUP_ID ?? 'event-processor-service',
+    redisUrl: process.env.REDIS_URL ?? 'redis://localhost:6379'
+  })
+)

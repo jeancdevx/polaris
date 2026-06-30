478d4c0dfb4ec6e99045866a5f133b0f34a0f4e7
jeancdev <jcode2006@gmail.com>
2026-06-30 17:16:08 -0500

    feat(sns): create SNS topic resource for operational alerts

---
+++ iac/modules/sns/topic.tf
@@ -0,0 +1,8 @@
+resource "aws_sns_topic" "alerts" {
+  name = "${local.name_prefix}-alerts"
+
+  tags = merge(local.common_tags, {
+    Name    = "${local.name_prefix}-alerts"
+    Purpose = "operational-alerts"
+  })
+}

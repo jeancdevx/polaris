b67e6accc7a8f4c8c564b5345aade4d5d921dcac
jeancdev <jcode2006@gmail.com>
2026-06-30 16:21:00 -0500

    feat(iot-core): add variables for sensor data processor Lambda and occupancy rules

--- iac/modules/iot-core/variables.tf
+++ iac/modules/iot-core/variables.tf
@@ -14,6 +14,31 @@ variable "project_name" {
   default     = "polaris"
 }
 
+variable "sensor_data_processor_function_arn" {
+  description = "ARN of the sensor-data-processor Lambda invoked by occupancy IoT rules"
+  type        = string
+}
+
+variable "sensor_data_processor_function_name" {
+  description = "Name of the sensor-data-processor Lambda invoked by occupancy IoT rules"
+  type        = string
+}
+
+variable "sensor_occupancy_rules" {
+  description = "IoT topic rules that route FC-51 occupancy telemetry to sensor-data-processor"
+  type = map(object({
+    description = string
+    topic       = string
+  }))
+
+  default = {
+    sensor_occupancy = {
+      description = "Route FC-51 occupancy changes to sensor-data-processor"
+      topic       = "parking/sensors/occupancy/+"
+    }
+  }
+}
+
 variable "rfid_validator_function_arn" {
   description = "ARN of the rfid-validator Lambda invoked by RFID IoT rules"
   type        = string

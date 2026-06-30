09ed258f07c6fcb4082244a2eccadd3767369e2c
jeancdev <jcode2006@gmail.com>
2026-06-30 15:17:59 -0500

    feat(admin-service): create UsersModule to encapsulate user management components and configuration

---
+++ apps/admin-service/src/users/users.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { CognitoAdminService } from './cognito-admin.service.js'
+import { DatabaseService } from './database.service.js'
+import { RfidValidationStore } from './rfid-validation.store.js'
+import { usersConfig } from './users.config.js'
+import { UsersController } from './users.controller.js'
+import { UsersRepository } from './users.repository.js'
+import { UsersService } from './users.service.js'
+
+@Module({
+  imports: [ConfigModule.forFeature(usersConfig)],
+  controllers: [UsersController],
+  providers: [
+    DatabaseService,
+    UsersRepository,
+    UsersService,
+    CognitoAdminService,
+    RfidValidationStore
+  ]
+})
+export class UsersModule {}

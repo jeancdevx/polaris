34e6a199c760381aa7ef25d064ee6689b9773be8
jeancdev <jcode2006@gmail.com>
2026-07-02 13:52:55 -0500

    feat(dev-iac): add ECR admin service module to Terraform configuration for development environment

--- iac/environments/dev/ecr.tf
+++ iac/environments/dev/ecr.tf
@@ -27,3 +27,13 @@ module "ecr_event_processor_service" {
 
   tags = var.tags
 }
+
+module "ecr_admin_service" {
+  source = "../../modules/ecr"
+
+  project_name = var.project_name
+  environment  = var.environment
+  service_name = "admin-service"
+
+  tags = var.tags
+}

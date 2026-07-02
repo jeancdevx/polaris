283d2de67c70259794e20e9bfe40a378eb6ea1f1
jeancdev <jcode2006@gmail.com>
2026-07-02 13:53:13 -0500

    feat(dev-iac): add ECS admin service Cognito and DynamoDB policies to IAM module in development environment

--- iac/environments/dev/iam.tf
+++ iac/environments/dev/iam.tf
@@ -4,9 +4,11 @@ module "iam" {
   project_name = var.project_name
   environment  = var.environment
 
-  msk_cluster_arn                       = module.kafka.cluster_arn
-  enable_ecs_api_service_cognito_policy = true
-  cognito_user_pool_arn                 = module.cognito.user_pool_arn
+  msk_cluster_arn                          = module.kafka.cluster_arn
+  enable_ecs_api_service_cognito_policy    = true
+  enable_ecs_admin_service_cognito_policy  = true
+  enable_ecs_admin_service_dynamodb_policy = true
+  cognito_user_pool_arn                    = module.cognito.user_pool_arn
 
   tags = var.tags
 }

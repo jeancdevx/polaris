c204f0f5f1ac2a5621be7c553af3b3f8fbe84e8c
jeancdev <jcode2006@gmail.com>
2026-07-02 18:43:22 -0500

    feat(appsync): create Lambda function for occupancy publisher with environment variables and tracing configuration

---
+++ iac/modules/appsync-occupancy-publisher/function.tf
@@ -0,0 +1,32 @@
+resource "aws_lambda_function" "main" {
+  function_name = local.function_name
+  role          = var.lambda_role_arn
+  handler       = "index.handler"
+  runtime       = "nodejs24.x"
+  timeout       = var.timeout_seconds
+  memory_size   = 256
+
+  filename         = data.archive_file.lambda_package.output_path
+  source_code_hash = data.archive_file.lambda_package.output_base64sha256
+
+  environment {
+    variables = {
+      APPSYNC_GRAPHQL_ENDPOINT     = var.appsync_graphql_endpoint
+      POWERTOOLS_LOG_LEVEL         = var.powertools_log_level
+      POWERTOOLS_METRICS_NAMESPACE = "Polaris"
+      POWERTOOLS_SERVICE_NAME      = local.function_name
+    }
+  }
+
+  tracing_config {
+    mode = var.enable_xray_tracing ? "Active" : "PassThrough"
+  }
+
+  tags = merge(local.common_tags, {
+    Name = local.function_name
+  })
+
+  depends_on = [
+    aws_cloudwatch_log_group.main,
+  ]
+}

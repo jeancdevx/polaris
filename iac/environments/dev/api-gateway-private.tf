044898e937a7d7b7174324af0473a2952f9f77a8
jeancdev <jcode2006@gmail.com>
2026-07-02 13:52:17 -0500

    feat(dev-iac): add Terraform module for private API gateway configuration in development environment

---
+++ iac/environments/dev/api-gateway-private.tf
@@ -0,0 +1,21 @@
+module "api_gateway_private" {
+  source = "../../modules/api-gateway-private"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  execute_api_vpc_endpoint_id = module.vpc.execute_api_vpc_endpoint_id
+  vpc_link_id                 = module.api_gateway.vpc_link_id
+  alb_listener_arn            = module.ecs.alb_listener_arn
+
+  cognito_app_client_id = module.cognito.app_client_id
+  cognito_issuer_url    = module.cognito.issuer_url
+
+  tags = var.tags
+
+  depends_on = [
+    module.api_gateway,
+    module.ecs,
+    module.vpc,
+  ]
+}

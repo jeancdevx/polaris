2301f58f23194cd92c88c56cb3cc1be4daf75ddd
jeancdev <jcode2006@gmail.com>
2026-07-03 06:30:06 -0500

    feat(route53): add AppSync domain name and API association resources

---
+++ iac/modules/edge/appsync-domain.tf
@@ -0,0 +1,17 @@
+resource "aws_appsync_domain_name" "graphql" {
+  count = var.appsync_api_id != "" ? 1 : 0
+
+  domain_name     = local.graphql_fqdn
+  certificate_arn = aws_acm_certificate_validation.regional.certificate_arn
+
+  tags = merge(local.common_tags, {
+    Name = local.graphql_fqdn
+  })
+}
+
+resource "aws_appsync_domain_name_api_association" "graphql" {
+  count = var.appsync_api_id != "" ? 1 : 0
+
+  api_id      = var.appsync_api_id
+  domain_name = aws_appsync_domain_name.graphql[0].domain_name
+}

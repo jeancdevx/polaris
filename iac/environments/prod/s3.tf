248ed60165663853d6469e98c8a3e87131ede52c
jeancdev <jcode2006@gmail.com>
2026-07-03 08:23:54 -0500

    feat(s3): add S3 module for production environment with necessary configurations

---
+++ iac/environments/prod/s3.tf
@@ -0,0 +1,14 @@
+module "s3" {
+  source = "../../modules/s3"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  alb_logs_prefix                   = var.alb_logs_prefix
+  force_destroy                     = var.s3_force_destroy
+  kms_key_arn                       = var.s3_kms_key_arn
+  lifecycle_glacier_transition_days = var.s3_lifecycle_glacier_transition_days
+  manage_assets_bucket_policy       = false
+
+  tags = var.tags
+}

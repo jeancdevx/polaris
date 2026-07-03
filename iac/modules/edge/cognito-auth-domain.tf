fa1e197ad0992b7108cf9bf3e964d65b900751fd
jeancdev <jcode2006@gmail.com>
2026-07-03 09:46:02 -0500

    fix(iac): add dependencies for Cognito user pool domain to ensure proper resource provisioning

--- iac/modules/edge/cognito-auth-domain.tf
+++ iac/modules/edge/cognito-auth-domain.tf
@@ -4,4 +4,9 @@ resource "aws_cognito_user_pool_domain" "auth" {
   domain          = local.auth_fqdn
   user_pool_id    = var.cognito_user_pool_id
   certificate_arn = aws_acm_certificate_validation.cloudfront.certificate_arn
+
+  depends_on = [
+    aws_acm_certificate_validation.cloudfront,
+    aws_route53_record.apex,
+  ]
 }

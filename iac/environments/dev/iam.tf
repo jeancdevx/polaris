25cdb7fedba43ac09490a10964f02c5f03c857a4
jeancdev <jcode2006@gmail.com>
2026-07-03 00:14:59 -0500

    feat(dev-iac): enable ECS database bootstrap Cognito policy in IAM configuration

--- iac/environments/dev/iam.tf
+++ iac/environments/dev/iam.tf
@@ -8,6 +8,7 @@ module "iam" {
   enable_ecs_api_service_cognito_policy    = true
   enable_ecs_admin_service_cognito_policy  = true
   enable_ecs_admin_service_dynamodb_policy = true
+  enable_ecs_db_bootstrap_cognito_policy   = true
   cognito_user_pool_arn                    = module.cognito.user_pool_arn
 
   tags = var.tags

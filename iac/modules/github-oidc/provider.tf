2cfc3abc0b57f0f1bfd279aec3f231bdbce648a2
jeancdev <jcode2006@gmail.com>
2026-07-02 23:38:41 -0500

    feat(github-oidc): add OpenID Connect provider resource for GitHub integration

---
+++ iac/modules/github-oidc/provider.tf
@@ -0,0 +1,11 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  count = var.create_oidc_provider ? 1 : 0
+
+  url             = "https://${local.oidc_hostname}"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+
+  tags = merge(local.common_tags, {
+    Name = "github-actions-oidc"
+  })
+}

597f162c2274da2053c1cac1804a8cbddb0ab348
jeancdev <jcode2006@gmail.com>
2026-07-03 08:05:33 -0500

    feat(api-gateway): add production API Gateway module with VPC integration and Cognito support

---
+++ iac/environments/prod/api-gateway.tf
@@ -0,0 +1,19 @@
+module "api_gateway" {
+  source = "../../modules/api-gateway"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  vpc_id                     = module.vpc.vpc_id
+  private_subnet_ids         = module.vpc.private_subnet_ids
+  vpc_link_security_group_id = module.security_groups.vpc_link_security_group_id
+
+  alb_listener_arn = module.ecs.alb_listener_arn
+
+  cognito_app_client_id = module.cognito.app_client_id
+  cognito_issuer_url    = module.cognito.issuer_url
+
+  cors_allow_origins = [local.admin_web_origin]
+
+  tags = var.tags
+}

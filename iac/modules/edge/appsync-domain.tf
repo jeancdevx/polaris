b458face83b11bf5369c9e5b475286a9888801bc
jeancdev <jcode2006@gmail.com>
2026-07-03 08:29:29 -0500

    fix(appsync): update domain name resource count condition

--- iac/modules/edge/appsync-domain.tf
+++ iac/modules/edge/appsync-domain.tf
@@ -1,12 +1,12 @@
 resource "aws_appsync_domain_name" "graphql" {
-  count = var.appsync_api_id != "" ? 1 : 0
+  count = var.enable_appsync_custom_domain ? 1 : 0
 
   domain_name     = local.graphql_fqdn
   certificate_arn = aws_acm_certificate_validation.regional.certificate_arn
 }
 
 resource "aws_appsync_domain_name_api_association" "graphql" {
-  count = var.appsync_api_id != "" ? 1 : 0
+  count = var.enable_appsync_custom_domain ? 1 : 0
 
   api_id      = var.appsync_api_id
   domain_name = aws_appsync_domain_name.graphql[0].domain_name

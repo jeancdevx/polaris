5af2e0609842007dc837cc9b089c2e1da6aef070
jeancdev <jcode2006@gmail.com>
2026-07-03 06:14:33 -0500

    feat(api-gateway): add API Gateway domain name and mapping resources

---
+++ iac/modules/edge/api-gateway-domain.tf
@@ -0,0 +1,19 @@
+resource "aws_apigatewayv2_domain_name" "api" {
+  domain_name = local.api_fqdn
+
+  domain_name_configuration {
+    certificate_arn = aws_acm_certificate_validation.regional.certificate_arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+
+  tags = merge(local.common_tags, {
+    Name = local.api_fqdn
+  })
+}
+
+resource "aws_apigatewayv2_api_mapping" "api" {
+  api_id      = var.api_gateway_id
+  domain_name = aws_apigatewayv2_domain_name.api.id
+  stage       = var.api_gateway_stage_name
+}

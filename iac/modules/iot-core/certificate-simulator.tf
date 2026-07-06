2ff57296bae303883c7f03fd07fc4f86ca518f87
jeancdev <jcode2006@gmail.com>
2026-07-05 22:05:12 -0500

    chore(iot-core): remove unused AWS IoT certificate simulator resources

--- iac/modules/iot-core/certificate-simulator.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_iot_certificate" "simulator" {
-  active = true
-}
-
-resource "aws_iot_policy_attachment" "simulator" {
-  policy = aws_iot_policy.device.name
-  target = aws_iot_certificate.simulator.arn
-}

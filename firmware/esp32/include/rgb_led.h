76249e4099ba4de6bf87362557c2bd21c3a5befe
jeancdev <jcode2006@gmail.com>
2026-07-05 22:18:20 -0500

    feat(esp32): add RgbLed class for RGB LED control and management

---
+++ firmware/esp32/include/rgb_led.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Arduino.h>
+
+enum class RgbMode : uint8_t {
+  Off,
+  Free,
+  Occupied,
+  BlinkGreen,
+};
+
+class RgbLed {
+ public:
+  RgbLed(int rPin, int gPin, int bPin);
+
+  void begin();
+  void setMode(RgbMode mode);
+  void update(unsigned long nowMs);
+
+ private:
+  int rPin_;
+  int gPin_;
+  int bPin_;
+  RgbMode mode_ = RgbMode::Off;
+  bool blinkOn_ = true;
+  unsigned long lastBlinkMs_ = 0;
+
+  void writeRaw(bool r, bool g, bool b) const;
+};

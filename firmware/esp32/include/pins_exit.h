6791a84fe49d75c8d09240c0379c4541ae4998b2
jeancdev <jcode2006@gmail.com>
2026-07-05 22:17:22 -0500

    feat(esp32): add pin configuration for RFID and servo control

---
+++ firmware/esp32/include/pins_exit.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace polaris::pins::exit {
+
+constexpr int kRfidSs = 5;
+constexpr int kRfidRst = 27;
+constexpr int kRfidSck = 18;
+constexpr int kRfidMiso = 19;
+constexpr int kRfidMosi = 23;
+
+constexpr int kServo = 13;
+
+}  // namespace polaris::pins::exit

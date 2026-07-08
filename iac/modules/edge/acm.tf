17847687448585855a198ea12bb0643ec04c7351
jeancdev <jcode2006@gmail.com>
2026-07-08 14:44:36 -0500

    feat(edge): conditionally manage ACM certificates and Route 53 records

--- iac/modules/edge/acm.tf
+++ iac/modules/edge/acm.tf
@@ -1,4 +1,6 @@
 resource "aws_acm_certificate" "regional" {
+  count = local.edge_full ? 1 : 0
+
   domain_name               = local.api_fqdn
   subject_alternative_names = local.acm_sans
   validation_method         = "DNS"
@@ -13,6 +15,8 @@ resource "aws_acm_certificate" "regional" {
 }
 
 resource "aws_acm_certificate" "cloudfront" {
+  count = local.edge_full ? 1 : 0
+
   provider = aws.us_east_1
 
   domain_name               = local.api_fqdn
@@ -29,13 +33,13 @@ resource "aws_acm_certificate" "cloudfront" {
 }
 
 resource "aws_route53_record" "cert_validation_regional" {
-  for_each = {
-    for option in aws_acm_certificate.regional.domain_validation_options : option.domain_name => {
+  for_each = local.edge_full ? {
+    for option in aws_acm_certificate.regional[0].domain_validation_options : option.domain_name => {
       name   = option.resource_record_name
       record = option.resource_record_value
       type   = option.resource_record_type
     }
-  }
+  } : {}
 
   zone_id         = var.hosted_zone_id
   name            = each.value.name
@@ -46,13 +50,13 @@ resource "aws_route53_record" "cert_validation_regional" {
 }
 
 resource "aws_route53_record" "cert_validation_cloudfront" {
-  for_each = {
-    for option in aws_acm_certificate.cloudfront.domain_validation_options : option.domain_name => {
+  for_each = local.edge_full ? {
+    for option in aws_acm_certificate.cloudfront[0].domain_validation_options : option.domain_name => {
       name   = option.resource_record_name
       record = option.resource_record_value
       type   = option.resource_record_type
     }
-  }
+  } : {}
 
   zone_id         = var.hosted_zone_id
   name            = each.value.name
@@ -63,13 +67,17 @@ resource "aws_route53_record" "cert_validation_cloudfront" {
 }
 
 resource "aws_acm_certificate_validation" "regional" {
-  certificate_arn         = aws_acm_certificate.regional.arn
+  count = local.edge_full ? 1 : 0
+
+  certificate_arn         = aws_acm_certificate.regional[0].arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation_regional : record.fqdn]
 }
 
 resource "aws_acm_certificate_validation" "cloudfront" {
+  count = local.edge_full ? 1 : 0
+
   provider = aws.us_east_1
 
-  certificate_arn         = aws_acm_certificate.cloudfront.arn
+  certificate_arn         = aws_acm_certificate.cloudfront[0].arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation_cloudfront : record.fqdn]
 }

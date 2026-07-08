80bb35d5fb137c2868af9e3b1ac1cdd63e8213c3
jeancdev <jcode2006@gmail.com>
2026-07-08 14:42:49 -0500

    feat(iac): enhance github-oidc module with S3 assets bucket ARN and CloudFront distribution ID dependencies

--- iac/environments/prod/github-oidc.tf
+++ iac/environments/prod/github-oidc.tf
@@ -5,5 +5,10 @@ module "github_oidc" {
   environment       = var.environment
   github_repository = var.github_repository
 
+  assets_bucket_arn              = module.s3.assets_bucket_arn
+  web_cloudfront_distribution_id = module.edge.web_cloudfront_distribution_id
+
   tags = var.tags
+
+  depends_on = [module.edge]
 }

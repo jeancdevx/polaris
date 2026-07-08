78e27cb7b20da70caaec15263ee93892f36579a0
jeancdev <jcode2006@gmail.com>
2026-07-08 14:42:16 -0500

    feat(iac): add AWS provider configuration for us-east-1 region in dev environment

--- iac/environments/dev/providers.tf
+++ iac/environments/dev/providers.tf
@@ -6,3 +6,13 @@ provider "aws" {
     tags = local.common_tags
   }
 }
+
+provider "aws" {
+  alias   = "us_east_1"
+  region  = "us-east-1"
+  profile = var.aws_profile != "" ? var.aws_profile : null
+
+  default_tags {
+    tags = local.common_tags
+  }
+}
